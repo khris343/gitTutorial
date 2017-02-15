0d66b21982f25e4c4f94876006e11bb1291eca16
khris343 <kwilhel2@uwo.ca>
2017-02-15 14:57:20 -0500

    Add files via upload

---
+++ gitTutorial/src/Course.java
@@ -0,0 +1,13 @@
+/**
+ * Created by khris on 2017-02-15.
+ */
+public class Course
+{
+    String cname;
+    String cdescription;
+    public Course(String courseName, String courseDescription)
+    {
+        this.cname = courseName;
+        this.cdescription = courseDescription;
+    }
+}
